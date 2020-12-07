91d361ffbdd3dd1b1862c16af87b848dbf6e5c56
DianaTerchea <tercheadianagabriela@yahoo.com>
2020-12-07 10:18:40 +0200

    Menu 2D

---
+++ Scene1/Assets/change_scene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_scene : MonoBehaviour
+{
+    public void chooseScene1()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+    public void chooseScene2()
+    {
+        SceneManager.LoadScene("SecondScene");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+ 
+};
