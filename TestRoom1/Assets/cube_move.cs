7a33abb5dbcbba7e346e0a58a56b0888ac006819
vlado1270 <andreivladserban@gmail.com>
2020-12-14 11:27:51 +0200

    Room1 Graphic Design

---
+++ TestRoom1/Assets/cube_move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
