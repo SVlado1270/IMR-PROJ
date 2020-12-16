ed09bf77ed456520e544c33fe7ded11d0a5b5467
DianaTerchea <tercheadianagabriela@yahoo.com>
2020-12-16 20:34:22 +0200

    Menu and planks

---
+++ TestRoom1/Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadRoom1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    
+    public void quitGame()
+    {
+
+        Application.Quit();
+    }
+}
