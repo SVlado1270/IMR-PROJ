6611e44ac7737d31f555aa6deddf300b96a36b28
DianaTerchea <tercheadianagabriela@yahoo.com>
2020-12-17 12:06:30 +0200

    Added audio link to menu/scene

---
+++ TestRoom1/Assets/MenuSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSettings : MonoBehaviour
+{
+    private AudioSource audioSrc;
+
+    private float musicVolume = 0f;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
---
+++ TestRoom1/Assets/musicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicController : MonoBehaviour
+{
+    private AudioSource audioSrc;
+
+    private float musicVolume = 0f;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        audioSrc.volume = PlayerPrefs.GetFloat("volume");
+    }
+
+    void Update()
+    {
+        audioSrc.volume = PlayerPrefs.GetFloat("volume");
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
